729fa4a28e52fc1f36669b0ca7086c11fdecaac1
Andrew-CC-Martin <amartin262@gmail.com>
2021-02-20 13:09:42 +1100

    add flag for CI pipeline integration

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,10 +9,6 @@ class PaymentsController < ApplicationController
     payment = Stripe::PaymentIntent.retrieve(payment_id)
     listing_id = payment.metadata.listing_id
     user_id = payment.metadata.user_id
-    puts "listing_id:"
-    p listing_id
-    puts "user_id:"
-    p user_id
 
     listing = Listing.find(listing_id.to_i)
     listing.sold = true
